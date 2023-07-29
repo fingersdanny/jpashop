db37219c31037968fc6fd7cdf16d6d5b4b646676
isevou__ <kbry12@naver.com>
2023-07-29 16:40:36 +0900

    add : table setting 3

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,4 +1,25 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    // Ordinal로 쓰면 int로 받음, 순서가 뒤바뀔 수 있기 때문에 꼭 String으로 할것.
+    private DeliveryStatus status;
 }
