14bf4b0137cc1a56910ba3b3e90df52c1ff9d453
isevou__ <kbry12@naver.com>
2023-07-30 18:41:12 +0900

    fix : MemberServiceTest 수정

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,7 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,20 +36,21 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test(expected = IllegalStateException.class)
+    @Test
     public void 중복_회원_예외() throws Exception {
         //given
         Member member1 = new Member();
-        member1.setName("kim1");
+        member1.setName("kim");
 
         Member member2 = new Member();
-        member2.setName("kim1");
+        member2.setName("kim");
 
         //when
         memberService.join(member1);
-        memberService.join(member2);
 
         //then
-        fail("예외가 발생해야 한다.");
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
     }
 }
