898eba295cd334de6cf634929dac21c7af7d8132
isevou__ <kbry12@naver.com>
2023-07-30 18:18:00 +0900

    fix : MemberRepository.java, MemberRepositoryTest.java

--- src/main/java/jpabook/jpashop/JpaBookApplication.java
+++ src/main/java/jpabook/jpashop/JpaBookApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpabookApplication {
+public class JpaBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpabookApplication.class, args);
+		SpringApplication.run(JpaBookApplication.class, args);
 	}
 }
