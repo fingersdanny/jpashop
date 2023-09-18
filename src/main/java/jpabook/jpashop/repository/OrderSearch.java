38c55a1d3565204343e4cf82d62928c63e128164
isevou__ <kbry12@naver.com>
2023-09-18 21:20:30 +0900

    feat : 상품 수정 및 취소

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,11 +1,12 @@
-//package jpabook.jpashop.repository;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//public class OrderSearch {
-//    private String member;
-//
-//}
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+}
